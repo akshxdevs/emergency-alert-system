85102a1c106ec1a33a84e4884b90deff6159a81a
akshxdevs <akshxdevs@gmail.com>
2025-08-12 14:19:44 +0530

    fe codebase editted

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,19 +1,7 @@
 "use client";
-import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 const myFont = localFont({
   src: './fonts/Gilroy-Medium.ttf',
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,16 +1,8 @@
-"use client";
-
-import { motion } from "framer-motion";
 import { LandingPage } from "./Components/LandingPage/LandingPage";
-
 export default function Home() {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.8, ease: "easeOut" }}
-    >
+    <div>
       <LandingPage/>
-    </motion.div>
+    </div>
   );
 }
